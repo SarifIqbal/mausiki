a4ce25d0fc1cfd24576f754177d97f241f558403
Sarif Iqbal <iqbalsarif.official@gmail.com>
2023-10-21 21:20:21 +0530

    Home and Search Buttons Added for Mobile View

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/header";
+
 export default function Home() {
     return (
-        <div className="text-blue-400">
-            Main Content
+        <div className="
+            bg-neutral-900
+            rounded-lg
+            h-full
+            w-full
+            overflow-hidden
+            overflow-y-auto
+        ">
+            <Header>
+                Header
+            </Header>
         </div>
     )
 }
