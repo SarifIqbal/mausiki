4119e757698f56f1ab33c9e16fe44ab89479748c
Sarif Iqbal <iqbalsarif.official@gmail.com>
2023-10-22 23:09:43 +0530

    Hook Creation Completed

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,6 +1,6 @@
 import { Subscription, UserDetails } from "@/types";
 import { User } from "@supabase/auth-helpers-nextjs";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react"
 
 type UserContextType = {
@@ -69,4 +69,12 @@ export const MyUderContextProvider = (props: Props) => {
     }
 
     return <UserContext.Provider value={value} {...props}/>
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider');
+    }
+    return context;
 };
