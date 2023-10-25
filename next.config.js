837416db6e6dee916818756bd766054cf4b04f80
Sarif Iqbal <iqbalsarif.official@gmail.com>
2023-10-25 22:37:18 +0530

    Programmed Song Play Button

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "aanbsppyqahtfcfpqakz.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
