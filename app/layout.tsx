542c4654327321bcdf7bb72feef2af7fbf5bd1fd
Sarif Iqbal <iqbalsarif.official@gmail.com>
2023-10-28 13:44:20 +0530

    Creating Player Content

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from '@/providers/userProvider';
 import ModalProvider from '@/providers/modalProvider';
 import ToasterProvider from '@/providers/toasterProvider';
 import getSongsByUserId from '@/actions/getSongsByUserId';
+import Player from '@/components/player';
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default async function RootLayout({
                         <Sidebar songs={userSongs}>
                             {children}
                         </Sidebar>
+                        <Player />
                     </UserProvider>
                 </SupabaseProvider>
             </body>
